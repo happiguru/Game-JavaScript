b7edb4d178fa1cc81855903bad081770d1dd3902
happiguru <hguruman@gmail.com>
2021-07-08 04:01:41 +0400

    Update file information

--- src/tests/submitScore.test.js
+++
@@ -1,8 +0,0 @@
-const SubmitScore = require('../modules/submitScore');
-
-it('submits the player score to the leaderboard', () => {
-  SubmitScore.send('PlayerOne', 0)
-    .then((response) => {
-      expect(response).toBe('Leaderboard score created correctly.');
-    });
-});
